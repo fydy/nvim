6fcbcbb42a657c1ef0152273cf0a0ce40b04f325
elaw <deweke@163.com>
2023-05-09 18:05:48 +0800

    变更

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -1,5 +1,5 @@
-lvim.builtin.which_key.mappings["y"] = { "<cmd>w !python<CR>", "执行当前python文件" }
-lvim.builtin.which_key.mappings["w"] = { "<cmd>w<CR>", "Save" }
+lvim.builtin.which_key.mappings["y"] = { "<cmd>w !python<cr>", "执行当前python文件" }
+lvim.builtin.which_key.mappings["w"] = { "<cmd>w<cr>", "Save" }
 lvim.builtin.which_key.mappings["b"] = { "<cmd>Telescope buffers<cr>", "Buffers" }
 lvim.builtin.which_key.mappings["v"] = { "<cmd>vsplit<cr>", "vsplit" }
 --lvim.builtin.which_key.mappings["h"] = { "<cmd>nohlsearch<cr>", "nohl" }
