e7fa14f7f403a72e9001f9064e1ebf77aae6e04a
elaw <deweke@163.com>
2023-05-09 15:55:41 +0800

    变更

--- config.lua
+++ config.lua
@@ -154,6 +154,44 @@ lvim.builtin.treesitter.highlight.enable = true
 lvim.builtin.nvimtree.active = false -- NOTE: using neo-tree
 --lvim.builtin.treesitter.matchup.enable = true
 lvim.builtin.treesitter.rainbow.enable = true
+lvim.builtin.treesitter.autotag.enable = true
+lvim.builtin.treesitter.auto_install = false
+
+lvim.builtin.treesitter.ignore_install = { "markdown" }
+
+lvim.builtin.treesitter.textobjects = {
+    select = {
+        enable = true,
+        -- Automatically jump forward to textobj, similar to targets.vim
+        lookahead = true,
+        keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["at"] = "@class.outer",
+            ["it"] = "@class.inner",
+            ["ac"] = "@call.outer",
+            ["ic"] = "@call.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+            ["ai"] = "@conditional.outer",
+            ["ii"] = "@conditional.inner",
+            ["a/"] = "@comment.outer",
+            ["i/"] = "@comment.inner",
+            ["ab"] = "@block.outer",
+            ["ib"] = "@block.inner",
+            ["as"] = "@statement.outer",
+            ["is"] = "@scopename.inner",
+            ["aA"] = "@attribute.outer",
+            ["iA"] = "@attribute.inner",
+            ["aF"] = "@frame.outer",
+            ["iF"] = "@frame.inner",
+        },
+    },
+}
+
 -- make sure server will always be installed even if the server is in skipped_servers list
 lvim.lsp.installer.setup.ensure_installed = {
     "jsonls",
@@ -295,23 +333,23 @@ lvim.plugins = {
             })
         end
     },
-  {
-    {nvim-tree/nvim-tree.lua},
-    config = function()
-      require("nvim-tree").setup({
-      sort_by = "case_sensitive",
-      view = {
-        width = 30,
-      },
-      renderer = {
-        group_empty = true,
-      },
-      filters = {
-        dotfiles = true,
-      },
-    })
-    end,
-  },
+    {
+        "nvim-tree/nvim-tree.lua",
+        config = function()
+            require("nvim-tree").setup({
+                sort_by = "case_sensitive",
+                view = {
+                    width = 30,
+                },
+                renderer = {
+                    group_empty = true,
+                },
+                filters = {
+                    dotfiles = true,
+                },
+            })
+        end,
+    },
     -- lsp
     {
         "L3MON4D3/LuaSnip",
