19fe587457c7b7bc86fd4ca79ce1ea3f87e358c1
elaw <deweke@163.com>
2023-05-09 10:52:43 +0800

    变更

--- config.lua
+++ config.lua
@@ -152,7 +152,7 @@ lvim.builtin.treesitter.highlight.enable = true
 
 -- generic LSP settings
 lvim.builtin.nvimtree.active = false -- NOTE: using neo-tree
-lvim.builtin.treesitter.matchup.enable = true
+--lvim.builtin.treesitter.matchup.enable = true
 lvim.builtin.treesitter.rainbow.enable = true
 -- make sure server will always be installed even if the server is in skipped_servers list
 lvim.lsp.installer.setup.ensure_installed = {
@@ -246,7 +246,40 @@ lvim.plugins = {
             })
         end,
     },
-
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("neo-tree").setup({
+                close_if_last_window = true,
+                window = {
+                    width = 30,
+                },
+                buffers = {
+                    follow_current_file = true,
+                },
+                filesystem = {
+                    follow_current_file = true,
+                    filtered_items = {
+                        hide_dotfiles = false,
+                        hide_gitignored = false,
+                        hide_by_name = {
+                            "node_modules"
+                        },
+                        never_show = {
+                            ".DS_Store",
+                            "thumbs.db"
+                        },
+                    },
+                },
+            })
+        end
+    },
     -- lsp
     {
         "L3MON4D3/LuaSnip",
@@ -254,6 +287,13 @@ lvim.plugins = {
         version = "^1", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
         -- install jsregexp (optional!).
         build = "make install_jsregexp"
+    },
+    {
+        "nvim-telescope/telescope-project.nvim",
+        event = "BufWinEnter",
+        setup = function()
+            vim.cmd [[packadd telescope.nvim]]
+        end,
     },
     -- Indent line
     { 'lukas-reineke/indent-blankline.nvim', lazy = true },
@@ -272,7 +312,9 @@ lvim.plugins = {
             require('nvim-autopairs').setup {}
         end
     },
-
+    {
+        "mrjones2014/nvim-ts-rainbow",
+    },
     -- LSP
     { 'neovim/nvim-lspconfig', lazy = true },
 
@@ -315,7 +357,95 @@ lvim.plugins = {
         event = 'VeryLazy',
         config = true,
     },
-
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+                RGB = true, -- #RGB hex codes
+                RRGGBB = true, -- #RRGGBB hex codes
+                RRGGBBAA = true, -- #RRGGBBAA hex codes
+                rgb_fn = true, -- CSS rgb() and rgba() functions
+                hsl_fn = true, -- CSS hsl() and hsla() functions
+                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            })
+        end,
+    },
+    {
+        "karb94/neoscroll.nvim",
+        event = "WinScrolled",
+        config = function()
+            require('neoscroll').setup({
+                -- All these keys will be mapped to their corresponding default scrolling animation
+                mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                             '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+                hide_cursor = true, -- Hide cursor while scrolling
+                stop_eof = true, -- Stop at <EOF> when scrolling downwards
+                use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+                respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+                cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+                easing_function = nil, -- Default easing function
+                pre_hook = nil, -- Function to run before the scrolling animation starts
+                post_hook = nil, -- Function to run after the scrolling animation ends
+            })
+        end
+    },
+    {
+        "ethanholz/nvim-lastplace",
+        event = "BufRead",
+        config = function()
+            require("nvim-lastplace").setup({
+                lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+                lastplace_ignore_filetype = {
+                    "gitcommit", "gitrebase", "svn", "hgcommit",
+                },
+                lastplace_open_folds = true,
+            })
+        end,
+    },
+    {
+        "kevinhwang91/nvim-bqf",
+        event = { "BufRead", "BufNew" },
+        config = function()
+            require("bqf").setup({
+                auto_enable = true,
+                preview = {
+                    win_height = 12,
+                    win_vheight = 12,
+                    delay_syntax = 80,
+                    border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+                },
+                func_map = {
+                    vsplit = "",
+                    ptogglemode = "z,",
+                    stoggleup = "",
+                },
+                filter = {
+                    fzf = {
+                        action_for = { ["ctrl-s"] = "split" },
+                        extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre", -- this will only start session saving when an actual file was opened
+        module = "persistence",
+        config = function()
+            require("persistence").setup {
+                dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
+                options = { "buffers", "curdir", "tabpages", "winsize" },
+            }
+        end,
+    },
+    {
+        "Pocco81/auto-save.nvim",
+        config = function()
+            require("auto-save").setup()
+        end,
+    },
     -- Markdown 预览
     {
         'iamcco/markdown-preview.nvim',
