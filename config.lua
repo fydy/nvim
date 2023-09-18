b1b1c8680140ee8ada48c3038880779e98edbec8
Jack Smith <mcdowell0613@student.cptc.edu>
2023-09-18 22:32:57 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -368,6 +368,11 @@ lvim.plugins = {
     },
     {
         "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
         config = function()
             require("nvim-tree").setup({
                 sort_by = "case_sensitive",
