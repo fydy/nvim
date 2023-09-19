8ca849e4cc8da166d19cadcf69c0d9d277b7c2b8
Jack Smith <joyo003538262@qq.com>
2023-09-19 18:47:09 +0800

    1

--- config.lua
+++ config.lua
@@ -347,110 +347,24 @@ end
 lvim.plugins = {
     {
         "neovim/nvim-lspconfig",
-        -- experimental
-        init = function()
-          -- disable lsp watcher. Too slow on macos
-          local ok, wf = pcall(require, "vim.lsp._watchfiles")
-          if ok then
-            wf._watchfunc = function()
-              return function() end
-            end
-          end
+        config = function()
+            require("lspconfig")[lua_ls].setup {}
         end,
-        opts = {
-          -- Automatically format on save
-          autoformat = true,
-          -- on nvim 0.10 trying experimental for golang
-          inlay_hints = { enabled = false },
-          servers = {
-            lua_ls = {
-              enabled = true,
-              cmd = { "/data/data/com.termux/files/usr/bin/lua-language-server" },
-              single_file_support = true,
-              settings = {
-                Lua = {
-                  workspace = {
-                    checkThirdParty = false,
-                  },
-                  completion = {
-                    workspaceWord = true,
-                    callSnippet = "Both",
-                  },
-                  misc = {
-                    parameters = {
-                      -- "--log-level=trace",
-                    },
-                  },
-                  hint = {
-                    enable = true,
-                    setType = false,
-                    paramType = true,
-                    paramName = "Disable",
-                    semicolon = "Disable",
-                    arrayIndex = "Disable",
-                  },
-                  doc = {
-                    privateName = { "^_" },
-                  },
-                  type = {
-                    castNumberToInteger = true,
-                  },
-                  diagnostics = {
-                    globals = { "vim" },
-                    disable = { "incomplete-signature-doc", "trailing-space" },
-                    -- enable = false,
-                    groupSeverity = {
-                      strong = "Warning",
-                      strict = "Warning",
-                    },
-                    groupFileStatus = {
-                      ["ambiguity"] = "Opened",
-                      ["await"] = "Opened",
-                      ["codestyle"] = "None",
-                      ["duplicate"] = "Opened",
-                      ["global"] = "Opened",
-                      ["luadoc"] = "Opened",
-                      ["redefined"] = "Opened",
-                      ["strict"] = "Opened",
-                      ["strong"] = "Opened",
-                      ["type-check"] = "Opened",
-                      ["unbalanced"] = "Opened",
-                      ["unused"] = "Opened",
-                    },
-                    unusedLocalExclude = { "_*" },
-                  },
-                  format = {
-                    enable = false,
-                    defaultConfig = {
-                      indent_style = "space",
-                      indent_size = "2",
-                      continuation_indent_size = "2",
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    
-      -- handle installation of linters, formatters etc
-      {
-        "williamboman/mason.nvim",
-        opts = function(_, opts)
-          vim.list_extend(opts.ensure_installed, {
-            "markdownlint",
-            "shellcheck",
-            "shfmt",
-            "stylua",
-            "tflint",
-            "tfsec",
-            "pyright",
-            "gopls",
-          })
+        -- Next, you can provide targeted overrides for specific servers.
+        ["lua_ls"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup {
+                cmd = { "/data/data/com.termux/files/usr/bin/lua-language-server" },
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" }
+                        }
+                    }
+                }
+            }
         end,
-      },
-    
+    },
     { "lunarvim/colorschemes", lazy = true, },
     {
         "Manas140/run.nvim",
