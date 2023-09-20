30e36e0f3dfe336b2acad75001c718956de4d359
elaw <deweke@163.com>
2023-09-20 19:55:28 +0800

    1

--- config.lua
+++ config.lua
@@ -143,7 +143,6 @@ lvim.builtin.which_key.mappings["dF"] = {
     "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Test Class DAP" }
 lvim.builtin.which_key.mappings["dS"] = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" }
 
-
 -- binding for switching
 lvim.builtin.which_key.mappings["C"] = {
     name = "Python",
