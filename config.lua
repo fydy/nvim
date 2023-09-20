dc9f885ce615745ef5c5b4b736d1ce550495738c
elaw <deweke@163.com>
2023-09-20 20:13:12 +0800

    1

--- config.lua
+++ config.lua
@@ -88,48 +88,48 @@ end
 -- setup debug adapter
 
 lvim.builtin.dap.active = true
---local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
---pcall(function()
---    require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
---end)
---
----- setup testing
---require("neotest").setup({
---    adapters = {
---        require("neotest-python")({
---            -- Extra arguments for nvim-dap configuration
---            -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
---            dap = {
---                justMyCode = false,
---                console = "integratedTerminal",
---            },
---            args = { "--log-level", "DEBUG", "--quiet" },
---            runner = "pytest",
---        })
---    }
---})
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function()
+    require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+end)
+
+-- setup testing
+require("neotest").setup({
+    adapters = {
+        require("neotest-python")({
+            -- Extra arguments for nvim-dap configuration
+            -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+            dap = {
+                justMyCode = false,
+                console = "integratedTerminal",
+            },
+            args = { "--log-level", "DEBUG", "--quiet" },
+            runner = "pytest",
+        })
+    }
+})
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader = "space"
-local dap = require('dap')
--- DAP Python
-dap.adapters.python = {
-    type = 'executable',
-    command = '/data/data/com.termux/files/usr/bin/python',
-    --args = { '-m', 'debugpy.adapter' };
-}
-
-dap.configurations.python = {
-    {
-        type = 'python',
-        request = 'launch',
-        name = "Launch file",
-        program = "${file}",
-        pythonPath = function()
-            return '/data/data/com.termux/files/usr/bin/python'
-        end,
-    },
-}
+--lvim.leader = "space"
+--local dap = require('dap')
+---- DAP Python
+--dap.adapters.python = {
+--    type = 'executable',
+--    command = '/data/data/com.termux/files/usr/bin/python',
+--    --args = { '-m', 'debugpy.adapter' };
+--}
+--
+--dap.configurations.python = {
+--    {
+--        type = 'python',
+--        request = 'launch',
+--        name = "Launch file",
+--        program = "${file}",
+--        pythonPath = function()
+--            return '/data/data/com.termux/files/usr/bin/python'
+--        end,
+--    },
+--}
 
 lvim.builtin.which_key.mappings["dm"] = { "<cmd>lua require('neotest').run.run()<cr>",
                                           "Test Method" }
@@ -475,11 +475,13 @@ end
 
 -- Additional Plugins
 lvim.plugins = {
-    --{ "ChristianChiarulli/swenv.nvim" },
-    --{ "stevearc/dressing.nvim" },
+    { "ChristianChiarulli/swenv.nvim" },
+    { "stevearc/dressing.nvim" },
+    { "mfussenegger/nvim-dap-python" },
+    { "nvim-neotest/neotest" },
+    { "nvim-neotest/neotest-python" },
+    --{ "mfussenegger/nvim-dap" },
     --{ "mfussenegger/nvim-dap-python" },
-    --{ "nvim-neotest/neotest" },
-    --{ "nvim-neotest/neotest-python" },
     { "christianchiarulli/telescope-tabs", branch = "chris" },
     { "lunarvim/colorschemes", lazy = true, },
     --{
